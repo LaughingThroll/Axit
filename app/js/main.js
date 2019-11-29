c8e54c7ef9a14e1c2ad65a422da1e14c1eba9a78
LaughingThroll <LaughingThroll@gmail.com>
2019-11-29 17:15:43 +0200

    added unname/future/advantages/price/response/design/contact/footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,28 @@
 $(function(){
-  
+
+  $('.header__btn').click(function(){
+    $(this).toggleClass('active');
+    $('.header__menu').toggleClass('active');
+  });
+
+  $('.tabs .tab').on('click', function(event) {
+    var id = $(this).attr('data-id');
+      $('.tabs').find('.tab-item').removeClass('active-tab').hide();
+      $('.tabs .tab__menu').find('.tab').removeClass('active');
+      $(this).addClass('active');
+      $('#'+id).addClass('active-tab').fadeIn(1000);
+      return false;
+    });
+
+  $('.box-slider').slick({
+      slidesToShow: 2,
+      slidesToScroll: 2,
+      infinite: false,
+      prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+      nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+      vertical: true,
+      verticalSwiping: true,
+      speed: 700
+
+  });  
 });
