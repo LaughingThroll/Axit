d7fc7770e2dbdfb57c80ab2ccc96bda700f16507
LaughingThroll <LaughingThroll@gmail.com>
2019-12-02 22:50:44 +0200

    added startFormSubmit

--- app/js/main.js
+++ app/js/main.js
@@ -1,22 +1,5 @@
 $(function () {
-
-
-  // function designBg() {
-  //   $('.header__banner').css({
-  //     'background-image': 'url(images/content/design-bg.jpg)',
-  //   });
-  // }
-  // function headerBg() {
-  //   $('.header__banner').css({
-  //     'background-image': 'url(images/content/header__bg.jpg)',
-
-  //   });
-  // }
-  // setInterval(designBg, 4000);
-  // setInterval(headerBg, 8000);
-
-
-  $('.header__lang a').click(function(){
+  $('.header__lang a').click(function () {
     $('.header__lang a').removeClass('active');
     $(this).toggleClass('active');
   });
@@ -30,17 +13,19 @@ $(function () {
     rows: 0
   });
 
- 
 
-  $(document).mouseup(function(e) {
+  // strat 
+  $(document).mouseup(function (e) {
     var $target = $(e.target);
-    if ($target.closest('.header::before').length == 0) {
-        $('.header__menu').removeClass('active');
-        $('.header__btn').removeClass('active');
-        event.stopPropagation();
+    if (!$target.closest('.header__top').length) {
+      $('.header__menu').removeClass('active');
+      $('.header__btn').removeClass('active');
     }
+    event.stopPropagation();
   });
-  
+  // end 
+  // функция которая убирает меню при клике вне ее, но есть один баг то что при клике на .header__top меню не убирается, а если сделать чтобы убиралось то не работает .header__btn что делать я не знаю подскажеш а?? 
+
   $('.header__btn').on('click', function () {
     $(this).toggleClass('active');
     $('.header__menu').toggleClass('active');
@@ -75,6 +60,71 @@ $(function () {
         }
       }]
   });
+
+
+  $(function validForm() {
+    var pattern = /^[a-z0-9_-]+@[a-z0-9-]+\.([a-z]{1-6}\.)?[a-z]{2,6}$/i,
+      form = $('#form__contact'),
+      mail = $('#email'),
+      name = $('input[name="name"]'),
+      subject = $('input[name="subject"]'),
+      message = $('textarea[name="message"]'),
+      validName = false,
+      validSubject = false,
+      validMessage = false,
+      validMail = false;
+
+// форма отправки сообщений 
+    form.submit(function () {
+      event.preventDefault();
+      if (name.val() != '') {
+        validName = true;
+        name.parent().removeClass('error');
+      } else {
+        validName = false;
+        name.parent().addClass('error');
+      }
+      if (subject.val() != '') {
+        validSubject = true;
+        subject.parent().removeClass('error');
+      } else {
+        validSubject = false;
+        subject.parent().addClass('error');
+      }
+      if (message.val() != '') {
+        validMessage = true;
+        message.parent().removeClass('error');
+      } else {
+        validMessage = false;
+        message.parent().addClass('error');
+      }
+      if (mail.val() != '') {
+        if (mail.val().search(pattern) == 0) {
+          validMail = true;
+          mail.parent().removeClass('error');
+        } else {
+          mail.parent().addClass('error');
+          validMail = false;
+        }
+      } else {
+        mail.attr('placeholder', 'Введите e-mail');
+        mail.parent().addClass('error');
+        validMail = false;
+      }
+
+      if (validName == true && validMail == true && validSubject == true &&
+        validMessage == true) {
+        console.log('Сообщения успешно отправлено');
+        mail.parent().removeClass('error');
+        name.parent().removeClass('error');
+        subject.parent().removeClass('error');
+        message.parent().removeClass('error');
+      } else {
+        console.log('Error');
+      }
+    });
+  });
+  // не закончена так как нужно оптимизировать а как я пока что не знаю сильно длинный код и еще  нету AJAX но скоро будет
 
 });
 
