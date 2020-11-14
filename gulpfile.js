fbc31bbc8358521d4f4bd042586d724344d882b5
LaughingThroll <LaughingThroll@gmail.com>
2020-11-14 11:23:45 +0200

    added: prod

--- gulpfile.js
+++ gulpfile.js
@@ -8,28 +8,29 @@ let gulp = require('gulp'),
     cssmin = require('gulp-cssmin'),
     del = require('del');
 
-gulp.task('sass', function(){
-    return  gulp.src('app/scss/style.scss')
-                .pipe(sass({outputStyle: 'compressed'}))
-                .pipe(rename({suffix : '.min'}))
-                .pipe(autoprefixer({overrideBrowserslist: ['last 8 versions']}))
-                .pipe(gulp.dest('app/css'))
-                .pipe(browserSync.reload({stream: true}))
+gulp.task('sass', function () {
+    return gulp.src('app/scss/style.scss')
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(autoprefixer({ overrideBrowserslist: ['last 8 versions'] }))
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({ stream: true }))
 });
 
-gulp.task('html', function(){
+gulp.task('html', function () {
     return gulp.src('app/*.html')
-        .pipe(browserSync.reload({stream: true}))
+        .pipe(browserSync.reload({ stream: true }))
 });
-gulp.task('php', function(){
+gulp.task('php', function () {
     return gulp.src('app/*.php')
-        .pipe(browserSync.reload({stream: true}))
+        .pipe(browserSync.reload({ stream: true }))
 });
-gulp.task('js', function(){
+
+gulp.task('js', function () {
     return gulp.src('app/js/*.js')
-        .pipe(browserSync.reload({stream: true}))
+        .pipe(browserSync.reload({ stream: true }))
 });
-gulp.task('style', function(){
+gulp.task('style', function () {
     return gulp.src([
         'node_modules/normalize.css/normalize.css',
         'node_modules/slick-carousel/slick/slick.css',
@@ -39,7 +40,7 @@ gulp.task('style', function(){
         .pipe(cssmin())
         .pipe(gulp.dest('app/css'))
 });
-gulp.task('script', function(){
+gulp.task('script', function () {
     return gulp.src([
         'node_modules/slick-carousel/slick/slick.js',
         'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js'
@@ -49,15 +50,15 @@ gulp.task('script', function(){
         .pipe(gulp.dest('app/js'))
 });
 
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', function () {
     browserSync.init({
         server: {
             baseDir: "app/"
-          }
+        }
     });
 });
 
-gulp.task('clean', async function(){
+gulp.task('clean', async function () {
     del.sync('dist')
 });
 
@@ -70,11 +71,13 @@ gulp.task('export', function () {
         .pipe(gulp.dest('dist/js'))
     let buildFonts = gulp.src('app/fonts/**/*.*')
         .pipe(gulp.dest('dist/fonts'))
+    let buildImages = gulp.src('app/images/**/*.*')
+        .pipe(gulp.dest('dist/images'))
 });
 
 gulp.task('build', gulp.series('clean', 'export'))
 
-gulp.task('watch', function(){
+gulp.task('watch', function () {
     gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'))
     gulp.watch('app/*.html', gulp.parallel('html'))
     gulp.watch('app/**/*.php', gulp.parallel('php'))
